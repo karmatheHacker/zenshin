25ff05d758d1e833d41861f0f0bbd93cfc1fb749
theking-crypto <k2573190@gmail.com>
2026-02-01 17:57:53 +0530

    Fix_Vite_Base_Path

--- Archive/Web Version/FRONTEND/vite.config.js
+++ Archive/Web Version/FRONTEND/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/zenshin",
+  base: "/",
   plugins: [react(), eslint()],
 });
